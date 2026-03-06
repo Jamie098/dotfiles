63065a70c04cfd6da3ead37a8dbad63da1bd6ffd
Jamie <accounts@jamiehartley.com>
2026-03-06 11:57:15 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+SRCDIR="$HOME/.local/src"
+GITHUB="git@github.com:Jamie098"
+
+# Install pacman packages
+sudo pacman -S --needed - < packages/pacman.txt
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git "$SRCDIR/yay"
+cd "$SRCDIR/yay" && makepkg -si --conconfirm
+cd -
+
+# Install yay packages
+yay -S --needed - < packages/yay.txt
+
+# Get suckless components
+for repo ln dwm st dmenu; do
+	git clone "$GITHUB/$repo.git" "$SRCDIR/$repo"
+	cd "$SRCDIR/$repo" && sudo make clean install
+	cd -
+done
+
+# Get dotfiles
+git clone "$GITHUB/dotfiles.git" "$SRCDIR/dotfiles"
+cd "$SRCDIR/dotfiles" && stow --target="$HOME" .
+
+echo "Done. Manual steps remaining:"
+echo "	- Install oh-my-zsh"
+echo "	- Set zsh as default shell"
+
+echo "End."
