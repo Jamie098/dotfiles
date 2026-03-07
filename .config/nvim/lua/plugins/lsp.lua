f51d1dde87c8395c4d534d32e538cffde4418428
Jamie <accounts@jamiehartley.com>
2026-03-07 11:36:35 +0000

    Added starting nvim config

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+  { "neovim/nvim-lspconfig" },
+  { "mason-org/mason.nvim", opts = {} },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "clangd" },
+    },
+  },
+}
