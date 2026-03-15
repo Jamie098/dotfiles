dcd85772c5e5c59e3460306faaa0246aa118b425
Jamie <accounts@jamiehartley.com>
2026-03-15 17:17:41 +0000

    Fixed install script

--- install.sh
+++ install.sh
@@ -22,7 +22,12 @@ yay -S --needed - < packages/yay.txt
 # Get suckless components
 for repo in dwl somebar someblocks; do
 	git clone "$GITHUB/$repo.git" "$SRCDIR/$repo"
-	cd "$SRCDIR/$repo" && sudo make install
+	cd "$SRCDIR/$repo"
+	if [ "$repo" == "somebar" ]; then
+		cp src/config.def.hpp src/config.hpp && menson setup build && ninja -C build && sudo ninja -C build install
+	else
+		sudo make install
+	fi
 	cd -
 done
 
