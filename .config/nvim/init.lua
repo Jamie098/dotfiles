2b91cdbfde2c8ec1938b36454b44f6f62344ee4f
Jamie <accounts@jamiehartley.com>
2026-04-01 13:18:02 +0100

    Added nvim support for rust

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,8 +7,15 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 
 require("config.lazy")
 require("nvim-tree").setup()
 
-vim.lsp.enable({ "lua_ls", "clangd", "bashls" })
+vim.lsp.enable({ "lua_ls", "clangd", "bashls", "rust_analyzer" })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true
+})
