8267cb778ad8c1973f5f9b11178abfc22995b35e
Jamie <accounts@jamiehartley.com>
2026-04-05 09:27:11 +0100

    Added telescope to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,12 @@ require("nvim-tree").setup()
 
 vim.lsp.enable({ "lua_ls", "clangd", "bashls", "rust_analyzer" })
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
--- .config/nvim/lua/plugins/spec1.lua
+++ .config/nvim/lua/plugins/spec1.lua
@@ -15,5 +15,14 @@ return {
   },
   {
     "nvim-tree/nvim-web-devicons"
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- optional but recommended
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
   }
 }
