2bc86ab7f13ad15b644d8339edc5969e9015ebea
Jamie <accounts@jamiehartley.com>
2026-04-11 07:09:19 +0100

    Install cleanup

--- .config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-if test -z "$WAYLAND_DISPLAY" -a (tty) = "/dev/tty1"
-  exec dwl -s 'dunst & somebar & someblocks'
-end
--- install.sh
+++ install.sh
@@ -22,13 +22,6 @@ yay -S --needed - < packages/yay.txt
 # Get suckless components
 for repo in dwl somebar someblocks; do
 	git clone "$GITHUB/$repo.git" "$SRCDIR/$repo"
-	cd "$SRCDIR/$repo"
-	if [ "$repo" == "somebar" ]; then
-		cp src/config.def.hpp src/config.hpp && menson setup build && ninja -C build && sudo ninja -C build install
-	else
-		sudo make install
-	fi
-	cd -
 done
 
 # Get dotfiles
